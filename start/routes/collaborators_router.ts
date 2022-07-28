74428f06f99f448462a2e6e978d36fd6c850b0c7
moisesmember <moisesmember@gmail.com>
2022-07-27 22:46:37 -0400

    feat: module login

--- start/routes/collaborators_router.ts
+++ start/routes/collaborators_router.ts
@@ -2,5 +2,6 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group( () => {  
     Route.post('/collaborators_add', 'CollaboratorsController.createDatas')
+    Route.post('/collaborators_login', 'CollaboratorsController.login')
     Route.resource('/collaborators', 'CollaboratorsController').apiOnly()
 }).prefix('/api');
